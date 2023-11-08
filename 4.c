e436361da9d5c364ea6d0d109b7c89ae749c802d
Kaustubhugale1 <139043722+Kaustubhugale1@users.noreply.github.com>
2023-11-08 23:21:46 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    
+    // Input first numbers
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    // Input second numbers
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    if (num2 != 0) {
+        // Perform division if the second number is not zero
+        float result = num1 / num2;
+        printf("Result of division: %.2f\n", result);
+    } else {
+        printf("Division not possible. The second number cannot be zero.\n");
+    }
+
+    return 0;
+}
+
